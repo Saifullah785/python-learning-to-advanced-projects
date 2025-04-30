6b49cc62dc8669a8e13639222f447f1a2320c84b
Saifullah785 <muhammadalisaif785@gmail.com>
2025-04-30 17:51:43 +0500

    project_09_date_30_04_2025

---
+++ project_09_desktop_notifier_using_python.py
@@ -0,0 +1,18 @@
+# from plyer import notification
+# import time
+
+
+# if __name__ == '__main__':
+
+#     while True:
+
+        
+#         notification.notify(
+#             title="*** Take Rest ***",
+#             message="🔔 Time for a quick break! Step away from the screen, stretch a bit, and refresh your mind for better focus.",
+#             app_icon="E:/python practice notes/Python projects begginner to advaced/rest_time.ico",
+#             timeout=10)
+#         time.sleep(30)
+
+
+    
